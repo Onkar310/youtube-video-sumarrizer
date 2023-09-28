f936b203ca2b29caafe2ed3b0b2332a3f639c21b
Onkar310 <onkarsangale135@gmail.com>
2023-09-28 13:18:08 +0530

    static file changes

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const { summary } = require("./controller/openAiController");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static("/public"));
+
+app.post("/openai/get_summary", summary);
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
-
-app.use(express.json());
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.send("<h1>Hellow world</h1>");
-});
-app.use("/", express.static("public"));
-
-app.post("/openai/get_summary", summary);
