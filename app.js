2024f3838aa4ab87abab80f00c818307db23f2db
Onkar310 <onkarsangale135@gmail.com>
2023-09-28 13:52:16 +0530

    path changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { summary } = require("./controller/openAiController");
-
+const path = require("path");
 const app = express();
 
 app.use(express.json());
